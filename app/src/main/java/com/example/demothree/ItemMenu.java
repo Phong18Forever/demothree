45b3f653c6d7dde9ed6706f11dc435e1551ead36
unknown <trungphongptit@gmail.com>
2021-02-23 23:45:38 +0900

    test create new branch from local

---
+++ app/src/main/java/com/example/demothree/ItemMenu.java
@@ -0,0 +1,14 @@
+package com.example.demothree;
+
+public class ItemMenu {
+    String itemName;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
+
--- app/src/main/java/com/example/demothree/MainActivity.java
+++ app/src/main/java/com/example/demothree/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.demothree;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.demothree.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ItemMenu itemMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        itemMenu = new ItemMenu();
+        itemMenu.setItemName("Từ Vựng");
+        mainBinding.setItemMenu(itemMenu);
     }
 }
