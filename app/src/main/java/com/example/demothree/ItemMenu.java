ad63cdd5c7a988501137a0659b1fb4af0af3cf96
Phong18Forever <79467837+Phong18Forever@users.noreply.github.com>
2021-02-26 19:12:00 +0900

    Revert "test list view"

--- app/src/main/java/com/example/demothree/ItemMenu.java
+++ app/src/main/java/com/example/demothree/ItemMenu.java
@@ -2,7 +2,6 @@ package com.example.demothree;
 
 public class ItemMenu {
     String itemName;
-    int itemIcon;
 
     public String getItemName() {
         return itemName;
@@ -10,14 +9,6 @@ public class ItemMenu {
 
     public void setItemName(String itemName) {
         this.itemName = itemName;
-    }
-
-    public int getItemIcon() {
-        return itemIcon;
-    }
-
-    public void setItemIcon(int itemIcon) {
-        this.itemIcon = itemIcon;
     }
 }
 
